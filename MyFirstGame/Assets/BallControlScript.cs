faa4ff551ced79ea6fe492bb3d22a58ca686fc87
STUDENT Kevin Lynch <Kevin.Lynch5@students.ittralee.ie>
2023-10-26 10:53:10 +0100

    Signed-off-by: STUDENT Kevin Lynch <Kevin.Lynch5@students.ittralee.ie>

---
+++ MyFirstGame/Assets/BallControlScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControlScript : MonoBehaviour
+{
+    Rigidbody rb;
+    float kickStrenght = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        rb.AddForce(kickStrenght * Vector3.up, ForceMode.Impulse);  
+    }
+}
